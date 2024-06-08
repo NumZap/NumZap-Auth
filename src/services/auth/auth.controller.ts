aeb70df9e577bf56f06bc7ba5a6cb28072e0d2dd
leeyeonwoo <74847519+superbigpro@users.noreply.github.com>
2024-06-08 12:12:31 +0900

    fix: add missing files

--- src/services/auth/auth.controller.ts
+++ src/services/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { signUpDTO } from './dto/jwt.dto';
+import { JwtLoginDTO, SignUpDTO } from './dto/jwt.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // 로그인
+  // 회원가입
   @Post('signup')
-  async signup(@Body() body: signUpDTO) {
+  async signup(@Body() body: SignUpDTO) {
+    return this.authService.signUp(body);
+  }
+
+  // 로그인
+  @Post('login')
+  async login(@Body() body: JwtLoginDTO) {
     return this.authService.validate(body);
   }
 }
